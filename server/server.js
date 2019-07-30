8e07402f69d965dfd9982da48d84b05a4b1de08b
Victor Pragana <victormerigue@gmail.com>
2019-07-30 19:03:05 -0300

    adding backend

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+let interval;
+io.on("connection", socket => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 10000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+const getApiAndEmit = async socket => {
+  try {
+    const res = await axios.get(
+      "https://api.darksky.net/forecast/db6337afb58f45b2fa1032c2aeff0637/-23.542977, -46.731579"
+    ); // Getting the data from DarkSky
+    console.log("res.data", res.data);
+    socket.emit("FromAPI", res.data.currently.temperature); // Emitting a new message. It will be consumed by the client
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
